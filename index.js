f9fbfb5567abad02286ad9952e3fa1ad3ad413f5
Vimal Saraswat <106457983+vimalsaraswat@users.noreply.github.com>
2024-09-18 12:34:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,24 +2,28 @@ import puppeteer from "puppeteer";
 import fs from "fs";
 
 let allSpeeches = [];
-let yearlist = [];
+
+const browser = await puppeteer.launch({
+  args: ["--no-sandbox"],
+  headless: false,
+  defaultViewport: false,
+});
+
+const page = await browser.newPage();
+
+page.setDefaultNavigationTimeout(10 * 60 * 1000);
+
+await page.goto("https://rbi.org.in/Scripts/BS_ViewSpeeches.aspx/", {
+  waitUntil: "domcontentloaded",
+});
+
+const yearList = await page.evaluate(() => {
+  return Array.from(document.querySelectorAll("div.accordionButton")).map(
+    (link) => link.innerText
+  );
+});
 
 const fetchYearSpeeches = async (year) => {
-  const browser = await puppeteer.launch({
-    args: ["--no-sandbox"],
-    headless: false,
-  });
-
-  const page = await browser.newPage();
-
-  page.setDefaultNavigationTimeout(10 * 60 * 1000);
-
-  await page.goto("https://rbi.org.in/Scripts/BS_ViewSpeeches.aspx/", {
-    waitUntil: "domcontentloaded",
-  });
-  //  await page.evaluate((yearList) => {
-  // },year)
-
   await page.evaluate((year) => {
     const element = document.getElementById(`${year}0`);
     if (element) {
@@ -36,13 +40,16 @@ const fetchYearSpeeches = async (year) => {
 
     for (let i = 0; i < speechList.length - 1; i += 2) {
       const dateElement = speechList[i].querySelector(".tableheader>b");
-      const valueElement = speechList[i + 1].querySelector("td .link2");
 
-      if (dateElement && valueElement) {
+      const valueElement = speechList[i + 1].querySelector("td .link2");
+      const pdfElement = speechList[i + 1].querySelector('a[target="_blank"]');
+
+      if (dateElement && valueElement && pdfElement) {
         const date = dateElement.innerText.trim();
         const value = valueElement.innerText.trim();
+        const pdf = pdfElement.getAttribute("href");
 
-        speeches.push({ date, value });
+        speeches.push({ date, value, pdf });
       }
     }
 
@@ -50,23 +57,26 @@ const fetchYearSpeeches = async (year) => {
   });
 
   allSpeeches.push(...speeches);
-
-  await browser.close();
 };
 
-for (let i = 2024; i > 2020; i--) {
+for (let i = 2024; i > 2023; i--) {
   await fetchYearSpeeches(i);
 }
 
-const writeStream = fs.createWriteStream("data.csv");
+// yearList.forEach(async (year) => {
+//   await fetchYearSpeeches(2024);
+//   // console.log(year);
+// });
+await browser.close();
 
+const writeStream = fs.createWriteStream("data.csv");
 const csvString = [
   ["date", "speech"],
   ...allSpeeches.map((item) => [item.date, item.value]),
 ]
   .map((e) => e.join(","))
   .join("\n");
-
 writeStream.write(csvString);
 
 console.log(allSpeeches);
+console.log(yearList);
